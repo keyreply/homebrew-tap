95aa1cfae0c546a7cf438d67b97c447233eebb0f
Max Xu <max@keyreply.com>
2026-05-28 12:26:51 +0800

    kira 0.30.0 (#6)
    Co-authored-by: GitHub Actions (kira-cloudflare) <actions@github.com>

--- Formula/kira.rb
+++ Formula/kira.rb
@@ -1,13 +1,13 @@
 class Kira < Formula
   desc "KeyReply Kira Platform CLI"
   homepage "https://github.com/keyreply/kira-cloudflare"
-  version "0.29.1"
+  version "0.30.0"
   license :cannot_represent
 
   on_macos do
     on_arm do
       url "https://github.com/keyreply/homebrew-tap/releases/download/v#{version}/kira-#{version}-darwin-arm64.tar.gz"
-      sha256 "4e98714251334e6461350ee515fafacc88dc5504db8edaea1925c69844fd0251"
+      sha256 "5ef11322f37e3e3aa386ab5abf3243635361c394a85f397637fb708006fa037b"
     end
   end
 
