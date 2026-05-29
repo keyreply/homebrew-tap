8ce790598de608ec8014a7261d8ab93a758b3abd
Max Xu <max@keyreply.com>
2026-05-29 11:41:36 +0800

    kira 0.30.1
    Update Kira CLI Homebrew formula to v0.30.1.

--- Formula/kira.rb
+++ Formula/kira.rb
@@ -1,13 +1,13 @@
 class Kira < Formula
   desc "KeyReply Kira Platform CLI"
   homepage "https://github.com/keyreply/kira-cloudflare"
-  version "0.30.0"
+  version "0.30.1"
   license :cannot_represent
 
   on_macos do
     on_arm do
       url "https://github.com/keyreply/homebrew-tap/releases/download/v#{version}/kira-#{version}-darwin-arm64.tar.gz"
-      sha256 "5ef11322f37e3e3aa386ab5abf3243635361c394a85f397637fb708006fa037b"
+      sha256 "54e45101685401b2cc7a14579638aaba84526174e71d1a34b3063c9b3358eecd"
     end
   end
 
